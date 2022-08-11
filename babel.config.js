35615c8167f1597a5a5f6fe85b28ef590264c5ed
studio_xlnce <one.xlnce@gmail.com>
2022-08-11 14:31:42 +0100

    Added Google Places Autocomplete API with search.

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,15 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ["tailwindcss-react-native/babel"],
+    plugins: [
+      "tailwindcss-react-native/babel",
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env"
+        },
+      ],
+    ],
   };
 };
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,13 @@
-import { View, Text, SafeAreaView, Image } from 'react-native'
-import React from 'react'
-import NavOptions from '../components/NavOptions'
+import { View, Text, SafeAreaView, Image } from 'react-native';
+import React from 'react';
+import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from "@env";
 
 const HomeScreen = () => {
     return (
-        <SafeAreaView className="h-full bg-white">
-            <View className="p-5">
+        <SafeAreaView className="h-full bg-white p-5">
+            <View className="px-5">
                 <Image
                     style={{
                         width: 100,
@@ -15,6 +17,31 @@ const HomeScreen = () => {
                     source={{
                         uri: "https://links.papareact.com/gzs"
                     }}
+                />
+
+                <GooglePlacesAutocomplete
+                    placeholder="Where From?"
+                    styles={{
+                        container: {
+                            flex: 0,
+                        },
+                        textInput: {
+                            fontSize: 18,
+                        },
+                    }}
+                    onPress={(data, details = nul) => {
+                        console.log(data);
+                        console.log(details);
+                    }}
+                    fetchDetails={true}
+                    enablePoweredByContainer={false}
+                    minLength={2}
+                    query={{
+                        key: GOOGLE_MAPS_APIKEY,
+                        language: 'en',
+                    }}
+                    nearbyPlacesAPI="GooglePlacesSearch"
+                    debounce={400}
                 />
 
                 <NavOptions />
