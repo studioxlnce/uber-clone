cd72c422eeba58dd9f56d5c0aed662c842d9e769
studio_xlnce <one.xlnce@gmail.com>
2022-08-11 07:20:12 +0100

    React Navigation and Stack imported Screens set up.

--- App.js
+++ App.js
@@ -1,16 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from "react-redux";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { TailwindProvider } from 'tailwindcss-react-native';
 import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
+import MapScreen from './screens/MapScreen';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
     <Provider store={store}>
-      <TailwindProvider>
-        <HomeScreen />
-      </TailwindProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <TailwindProvider>
+            <Stack.Navigator>
+              <Stack.Screen
+                name="HomeScreen"
+                component={HomeScreen}
+                options={{
+                  headerShown: false,
+                }}
+              />
+              <Stack.Screen
+                name="MapScreen"
+                component={MapScreen}
+                options={{
+                  headerShown: false,
+                }}
+              />
+            </Stack.Navigator>
+          </TailwindProvider>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
---
+++ components/NavOptions.js
@@ -0,0 +1,48 @@
+import { View, Text, FlatList, TouchableOpacity, Image } from 'react-native';
+import React from 'react';
+import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
+
+const data = [
+    {
+        id: "123",
+        title: "Get a ride",
+        image: "https://links.papareact.com/3pn",
+        screen: "MapScreen",
+    },
+    {
+        id: "456",
+        title: "Order food",
+        image: "https://links.papareact.com/28w",
+        screen: "OrderScreen",
+    },
+]
+
+const NavOptions = () => {
+    const navigation = useNavigation();
+
+    return (
+        <FlatList
+            data={data}
+            keyExtractor={(item) => item.id}
+            horizontal
+            renderItem={({ item }) => (
+                <TouchableOpacity
+                    onPress={() => navigation.navigate(item.screen)}
+                    className="w-40 bg-gray-200 p-2 pl-6 pb-8 pt-4 m-2"
+                >
+                    <View>
+                        <Image
+                            style={{ width: 120, height: 120, resizeMode: "contain" }}
+                            source={{ uri: item.image }}
+                        />
+                        <Text className="mt-2 text-lg font-semibold">{item.title}</Text>
+                        <Icon className="w-10 bg-black rounded-full p-2 mt-4" name="arrowright" color="white" type="antdesign" />
+                    </View>
+                </TouchableOpacity>
+            )}
+        />
+    )
+}
+
+export default NavOptions
